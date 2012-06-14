2d3cde0adf09ba779c98b912d2a9421c942712bd
Jarrell Waggoner <malloc47@gmail.com>
2012-06-14 16:08:17 -0400

    previous commit already incorporated, but this will stay around for posterity and later use

--- adj/setup.py
+++
@@ -1,9 +0,0 @@
-#-------------------------------------------------------------------------------
-from distutils.core import setup, Extension;
-import numpy as np;
-#-------------------------------------------------------------------------------
-setup(name="adjc",
-      ext_modules=[Extension("adjc",sources=["adjc.cpp","RegionGraph.cpp"],
-                             include_dirs=[np.get_include()],
-                             extra_compile_args=["-fpermissive"])]);
-#-------------------------------------------------------------------------------
