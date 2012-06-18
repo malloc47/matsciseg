07c91c260f2bb7caa913f28272d8535d840463cb
Jarrell Waggoner <malloc47@gmail.com>
2012-06-18 19:23:36 -0400

    module for logging

---
+++ webgui/js/log.js
@@ -0,0 +1,31 @@
+var log = (function () {
+
+    var output;
+    var output_box;
+
+    function init(o,ob){
+	output = o;
+	output_box = ob;
+    }
+
+    function append(str){
+	if(str.indexOf('error:') === 0)
+	    output.append('<li class="errormsg">'+
+				// '['+new Date().toTimeString()+'] '+
+				str+'</li>');
+	else
+	    output.append('<li>'+
+				// '['+new Date().toTimeString()+'] '+
+				str+'</li>');
+	output_box.prop({ scrollTop: output_box.prop("scrollHeight") }, 30);
+    }
+
+    function clear(){output.children().remove();}
+
+    return {
+	init		: init,
+	append		: append,	
+	clear		: clear
+    }
+
+}());
