3ada03bb06ab0ba6c5e9f92f956973d52e71069c
Jarrell Waggoner <malloc47@gmail.com>
2012-06-18 21:03:47 -0400

    shifted more functionality to tools.js

--- webgui/js/tools.js
+++ webgui/js/tools.js
@@ -3,41 +3,68 @@ var tools = (function () {
     var toolMode;
     var imgMode;
 
+    var data = {'addition' : [],
+		'removal'  : []}
+
     var imgPaths = {'img' : 'empty',
 		     'seg' : 'output',
 		     'edg' : 'edge'};
 
-    var cursorMode = {'none':{'cursor':'default'},'addition':{'cursor':'crosshair'},'removal':{'cursor':'move'}};
+    var cursorMode = {'none':{'cursor':'default'},
+		      'addition':{'cursor':'crosshair'},
+		      'removal':{'cursor':'move'}};
+
     var currentCursor;
 
     function init() {
 	toolMode = 'none';
 	imgMode = 'seg';
 	currentCursor = 'default';
-    }
+	addition = [];
+	removal = [];
+    };
 
-    function getTool() {return toolMode;}
+    function getTool() {return toolMode;};
 
     function setTool(newToolMode) {
 	toolMode = toolMode==newToolMode ? 'none' : newToolMode;
 	currentCursor = cursorMode[toolMode];
-    }
+    };
 
-    function getImgMode() {return imgMode;}
+    function getImgMode() {return imgMode;};
 
-    function setImgMode(newImgMode) {imgMode = newImgMode;}
+    function setImgMode(newImgMode) {imgMode = newImgMode;};
 
-    function getImgPath() {return imgPaths[imgMode];}
+    function getImgPath() {return imgPaths[imgMode];};
 
-    function cursor() {return currentCursor;}
+    function cursor() {return currentCursor;};
+
+    function push(val,type) {
+	if(!type) type = toolMode;
+	data[type].push(val);
+    };
+
+    function clear() {
+	for(var d in data) {
+	    data[d] = [];
+	}
+    };
+
+    function get(type) {
+	if(!type) type = toolMode;
+	return data[type];
+    };
 
     return {
 	init		: init,
-	getTool		: getTool,
-	setTool		: setTool,
+	getTool	: getTool,
+	setTool	: setTool,
 	getImgMode	: getImgMode,
 	setImgMode	: setImgMode,
 	getImgPath	: getImgPath,
 	cursor		: cursor,
+	push		: push,
+	clear		: clear,
+	get		: get
     }
 }());
