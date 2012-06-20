fixed some bugs, make the cleaning more robust, and accelerated the circle masking

adj.py
@@ -2,6 +2,7 @@
 import numpy as np
 import scipy
 from scipy import ndimage
+import gcoc
 
 def adjacent(labels):
     """determine which labels are adjacent"""
@@ -44,9 +45,30 @@ def adjacent(labels):
 
     return adj
 
+def circle(p,shape):
+    output = np.zeros(shape,dtype='bool')
+    r = p[2]
+    cx, cy = p[0:2]
+    y, x = np.ogrid[-r:r+1, -r:r+1]
+    index = x**2 + y**2 <= r**2
+    # handle border cases
+    crop = ((cx-r)-max(cx-r,0), 
+            min(cx+r+1,output.shape[0])-(cx+r+1),
+            (cy-r)-max(cy-r,0),
+            min(cy+r+1,output.shape[1])-(cy+r+1))
+    output[cx-r-crop[0]:cx+r+crop[1]+1, cy-r-crop[2]:cy+r+crop[3]+1][
+        index[abs(crop[0]):index.shape[0]-abs(crop[1]),
+              abs(crop[2]):index.shape[1]-abs(crop[3])]] = True
+    return output
+
+# def circle_mask(r,shape,p):
+#     y,x = np.ogrid[-shape[0]:shape[0], -shape[1]:shape[1]]
+#     return (x-(p[0]-shape[0]*2))**2+(y-(p[1]-shape[1]*2))**2 <= r**2
+
 class Adj(object):
     def __init__(self,labels):
-        self.v = adjacent(labels.v)
+        self.v = gcoc.adjacent(labels.v,labels.max()+1)
+        # self.v = adjacent(labels.v)
     
     def get_adj(self,label_list):
         """return all labels that are adjacent to label_list labels"""
@@ -57,14 +79,11 @@ class Adj(object):
             output += [i for i,x in enumerate(self.v[l,:]) if x]
         return set(output)
 
-    # update
+    # doesn't really belong in adj, per-se
     def get_adj_radius(self,p,labels):
-        import gui
-        output = set()
-        for i,v in np.ndenumerate(labels):
-            if gui.dist(i,p[0:2]) < p[2]:
-                output.add(labels[i])
-        return output
+        """get all regions covered by circle"""
+        return set(np.unique(labels[
+                    circle(p,labels.shape)]))
 
     def set_adj_new(self,adj=[]):
         """ add new row and column to self.v for new label"""

gco.py
@@ -94,6 +94,9 @@ class Slice(object):
         v.graph_cut(1)
         return v
 
+    # from profilehooks import profile
+
+    # @profile
     def add_label(self,p,d):
         v = self.crop(list(self.adj.get_adj_radius(p,self.labels.v)))
         p = (p[0]-v.win[0],p[1]-v.win[1],p[2])
@@ -108,9 +111,7 @@ class Slice(object):
     def add_label_circle(self,p):
         import gui
         new_label = self.labels.max()+1
-        for i,v in np.ndenumerate(self.labels.v):
-            if gui.dist(i,p[0:2]) < p[2]:
-                self.labels.v[i] = new_label
+        self.labels.v[adj.circle(p,self.labels.v.shape)] = new_label
         # reconstruct data term after adding label
         # todo: do we really need to do these two lines?
         self.data = data.Data(self.labels.v)
@@ -160,8 +161,13 @@ class Slice(object):
             else:
                 # print("Shifting "+str(l)+" to "+str(new_label))
                 u[v.labels.v==l]=new_label
-                self.adj.set_adj_new(shifted_labels)
+                # self.adj.set_adj_new(shifted_labels)
                 new_label+=1
+                self.__init__(self.img, 
+                      self.labels.v, 
+                      self.shifted, 
+                      self.win, 
+                      self.mask)
 
         # self.__init__(self.img,
         #               region_clean(region_shift(self.labels,
@@ -179,6 +185,10 @@ class Slice(object):
             print("Not all pixels have a label")
         else:
             print("All pixels have a label")
+
+        print("region size: " + str(self.img.shape))
+        print("min: " + str(self.labels.v.min()))
+        print("max: " + str(self.labels.v.max()))
 
         output = gcoc.graph_cut(self.data.matrix(),
                                 self.img,

label.py
@@ -59,8 +59,30 @@ def largest_connected_component(im):
     sizes = [ ((labels==l).sum(),l) for l in range(1,num+1) ]
     return labels==max(sizes,key=lambda x:x[0])[1]
 
+def small_filter(labels,label_num):
+    """cover small label region with the nearest label"""
+    mask = create_mask(labels,[label_num]);
+    if not mask.any():
+        return labels
+    (x0,y0,x1,y1) = fit_region_z(mask);
+    if (x0>0): x0 -= 1;
+    if (y0>0): y0 -= 1;
+    if (x1<labels.shape[1]-1): x1 += 1;
+    if (y1<labels.shape[0]-1): y1 += 1;
+    chk_win = mask[y0:y1+1,x0:x1+1];
+
+    inds = scipy.ndimage.morphology.distance_transform_edt(chk_win,
+                                                           return_distances=False,
+                                                           return_indices=True);
+
+    for y in range(0,chk_win.shape[0]):
+        for x in range(0,chk_win.shape[1]):
+            labels[y+y0,x+x0] = labels[inds[0][y,x]+y0,inds[1][y,x]+x0]
+
+    return labels
+
 def region_clean(regions):
-    out = np.zeros(regions.shape,dtype=regions.dtype)
+    out = np.ones(regions.shape,dtype=regions.dtype)*-1
     for l in range(regions.max()+1) :
         layer = (regions==l)
         if layer.any() :
@@ -68,8 +90,9 @@ def region_clean(regions):
             labeled = largest_connected_component(layer>0)
             # copy only the largest connected component
             out[np.nonzero(labeled == label_max(labeled))] = l
-    # todo: what's happening with the zeros?
-    return out
+    return small_filter(out,-1)
+    # todo: what's happening with the -1s?
+    # return out
 
 def fit_region_z(im):
     """similar to fit_region"""
@@ -85,26 +108,6 @@ class Label(object):
     def create_mask(self,label_list):
         """get binary mask from a list of labels (in an integer matrix)"""
         return reduce(np.logical_or,map(lambda l:self.v==l,label_list))
-
-    def small_filter(self,label_num):
-        """cover small label region with the nearest label"""
-        mask = self.v.create_mask([label_num]);
-        (x0,y0,x1,y1) = fit_region_z(mask);
-        if (x0>0): x0 -= 1;
-        if (y0>0): y0 -= 1;
-        if (x1<self.v.shape[1]-1): x1 += 1;
-        if (y1<self.v.shape[0]-1): y1 += 1;
-        chk_win = mask[y0:y1+1,x0:x1+1];
-
-        inds = scipy.ndimage.morphology.distance_transform_edt(chk_win,
-                                                               return_distances=False,
-                                                               return_indices=True);
-
-        for y in range(0,chk_win.shape[0]):
-            for x in range(0,chk_win.shape[1]):
-                self.v[y+y0,x+x0] = self.v[inds[0][y,x]+y0,inds[1][y,x]+x0]
-
-        return self.v
 
     def max(self):
         return self.v.max()