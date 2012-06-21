c0132ace564eb161ae2ebf1c7ddecb9e8a9d7ee6
Jarrell Waggoner <malloc47@gmail.com>
2012-06-21 01:54:34 -0400

    real removal function added

--- gco.py
+++ gco.py
@@ -18,13 +18,16 @@ def smoothFn(s1,s2,l1,l2,adj):
     # return int(1.0/float((abs(float(s1)-float(s2)) if \
         # abs(float(s1)-float(s2)) > 9 else 9)+1)*255.0)
 
+def crop_box(a,box):
+    (x0,y0,x1,y1) = box
+    return a[y0:y1, x0:x1]
+
 class Slice(object):
     def __init__(self, img, labels, shifted={}, 
                  win=(0,0), mask=None, lightweight=False,
                  nodata=False):
         """initialize fields and compute defaults"""
-        # These values are created
-        # when the class is instantiated.
+        # These values are created when the class is instantiated.
         self.img = img.copy()
         if lightweight:
             self.labels = label.Label()
@@ -41,7 +44,6 @@ class Slice(object):
         self.adj = adj.Adj(self.labels) # adjacent(self.labels)
         self.shifted=shifted
         self.win=win
-        self.shifted=shifted
         self.mask=mask
 
     def edit_labels_gui(self,d):
@@ -60,10 +62,6 @@ class Slice(object):
         remove = set([self.labels[(b,a)] for a,b in removal])
         self.process_annotations(create,remove,d)
 
-    from profilehooks import profile
-    # from profilehooks import timecall
-
-    # @profile(immediate=True)
     def process_annotations(self,create,remove,d):
         new_volumes = []
         for r in remove:
@@ -81,8 +79,9 @@ class Slice(object):
                       , lightweight=True
                       , nodata=True)
 
-    @profile(immediate=True)
-    def remove_label(self,l,d):
+    def remove_label_dilation(self,l,d):
+        """removal that uses a large dilation to (hopefully) remove
+        the candidate region"""
         v = self.crop([l])
         v.data.dilate_all(d)
         v.data.label_inexclusive(v.labels.v,l)
@@ -90,14 +89,27 @@ class Slice(object):
         v.graph_cut(1,lite=True)
         return v
 
-    @profile(immediate=True)
+    def remove_label(self,l,d):
+        """removal that truly removes region"""
+        v = self.crop([l])
+        v.data.or_all(crop_box(data.select_region(self.labels.v,l),
+                               (v.win[1]
+                                , v.win[0]
+                                , v.win[1] + v.labels.v.shape[1]
+                                , v.win[0] + v.labels.v.shape[0])),
+                      skip=[0])
+        new_l = (key for key,value in v.shifted.items() if value==l).next()
+        v.data.label_erase(new_l)
+        v.adj.set_adj_all()
+        v.graph_cut(1,lite=True)
+        return v
+
     def add_label(self,p,d):
         v = self.crop(list(self.adj.get_adj_radius(p,self.labels.v)))
         p = (p[0]-v.win[0],p[1]-v.win[1],p[2])
         l = v.add_label_circle(p)
         v.data.dilate_label(l,p[2]*d)
         v.data.label_exclusive(v.labels.v,l)
-        # v.output_data_term()
         v.adj.set_adj_label_all(l)
         v.graph_cut(1,lite=True)
         return v
@@ -107,7 +119,7 @@ class Slice(object):
         new_label = self.labels.max()+1
         self.labels.v[adj.circle(p,self.labels.v.shape)] = new_label
         # reconstruct data term after adding label
-        # todo: do we really need to do these two lines?
+        # todo: constructing these before this is useless work
         self.data = data.Data(self.labels.v)
         self.adj = adj.Adj(self.labels)
         return new_label
@@ -115,28 +127,26 @@ class Slice(object):
     def crop(self,label_list):
         """fork off subwindow volume"""
         label_list = self.adj.get_adj(label_list)
-        # import code; code.interact(local=locals())
         mask = self.labels.create_mask(label_list)
-        (x0,y0,x1,y1) = label.fit_region(mask)
+        box = label.fit_region(mask)
         # crop out everything with the given window
-        mask_cropped = mask[y0:y1, x0:x1]
-        cropped_seed = self.labels.v[y0:y1, x0:x1]
-        new_img = self.img[y0:y1, x0:x1]
+        mask_cropped = crop_box(mask,box)
+        cropped_seed = crop_box(self.labels.v,box)
+        new_img      = crop_box(self.img,box)
         # transform seed img
-        # new_seed = np.zeros(cropped_seed.shape).astype('int16')
         new_seed = np.zeros_like(cropped_seed)
         label_transform = {}
         new_label = 1
-        new_seed[np.logical_not(mask_cropped)] = 0
         for l in label_list:
             label_transform[new_label]=l
-            # label_transform.append((l,new_label))
+            # print("Shifting "+str(l)+" to "+str(new_label))
             new_seed[cropped_seed==l] = new_label
             new_label += 1
+        # new_seed[np.logical_not(mask_cropped)] = 0
         return Slice(new_img
                      , new_seed
                      , label_transform
-                     , (y0,x0)
+                     , (box[1],box[0])
                      , mask_cropped
                      , lightweight=True)
 
@@ -175,8 +185,8 @@ class Slice(object):
             print("All pixels have a label")
 
         print("region size: " + str(self.img.shape))
-        print("min: " + str(self.labels.v.min()))
-        print("max: " + str(self.labels.v.max()))
+        # print("min: " + str(self.labels.v.min()))
+        # print("max: " + str(self.labels.v.max()))
 
         output = gcoc.graph_cut(self.data.matrix(),
                                 self.img,
