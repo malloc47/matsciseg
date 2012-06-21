085d931bcd7452a3ae4c9940ba03edfdf7ab6a0a
Jarrell Waggoner <malloc47@gmail.com>
2012-06-21 16:31:12 -0400

    script that pickles up all the relevant stuff for the demo

---
+++ tests/webgui-pickle.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+./pickle_labels.py ti.pkl 90 seq1/img/image0090.png seq1/global-20/90/image0090.label \
+	91 seq1/img/image0091.png seq1/global-20/90/image0091.label \
+	92 seq1/img/image0092.png seq1/global-20/90/image0092.label \
+	93 seq1/img/image0093.png seq1/global-20/90/image0093.label \
+	94 seq1/img/image0094.png seq1/global-20/90/image0094.label \
+	95 seq1/img/image0095.png seq1/global-20/90/image0095.label \
+	96 seq1/img/image0096.png seq1/global-20/90/image0096.label \
+	97 seq1/img/image0097.png seq1/global-20/90/image0097.label \
+	98 seq1/img/image0098.png seq1/global-20/90/image0098.label \
+	99 seq1/img/image0099.png seq1/global-20/90/image0099.label \
+       100 seq1/img/image0100.png seq1/global-20/90/image0100.label
+
+./pickle_labels.py ti2.pkl 90 seq1/img/image0090.png seq1/global-20/90/image0090.label \
+	91 seq1/img/image0091.png seq1/global-20/90/image0091.label
+
+./pickle_labels.py ti4.pkl 90 seq1/img/image0090.png seq1/global-20/90/image0090.label \
+	91 seq1/img/image0091.png seq1/global-20/90/image0091.label \
+	92 seq1/img/image0092.png seq1/global-20/90/image0092.label \
+	93 seq1/img/image0093.png seq1/global-20/90/image0093.label 
+
+./pickle_labels.py c1a.pkl 90 seq1/cropped/image0090.png seq1/cropped/image0090.label \
+	91 seq1/cropped/image0091.png seq1/cropped/image0091.label \
+	92 seq1/cropped/image0092.png seq1/cropped/image0092.label \
+	93 seq1/cropped/image0093.png seq1/cropped/image0093.label \
+	94 seq1/cropped/image0094.png seq1/cropped/image0094.label \
+	95 seq1/cropped/image0095.png seq1/cropped/image0095.label \
+	96 seq1/cropped/image0096.png seq1/cropped/image0096.label \
+	97 seq1/cropped/image0097.png seq1/cropped/image0097.label \
+	98 seq1/cropped/image0098.png seq1/cropped/image0098.label \
+	99 seq1/cropped/image0099.png seq1/cropped/image0099.label \
+       100 seq1/cropped/image0100.png seq1/cropped/image0100.label
+
+./pickle_labels.py c1b.pkl 90 seq1/cropped/image0090.png seq1/cropped/image0090.label \
+	91 seq1/cropped/image0091.png seq1/cropped/image0091.label \
+	92 seq1/cropped/image0092.png seq1/cropped/image0092.label \
+	93 seq1/cropped/image0093.png seq1/cropped/image0093.label \
+	94 seq1/cropped/image0094.png seq1/cropped/image0094.label \
+	95 seq1/cropped/image0095.png seq1/cropped/image0095.label
+
+./pickle_labels.py c2a.pkl 90 seq1/cropped2/image0090.png seq1/cropped2/image0090.label \
+	91 seq1/cropped2/image0091.png seq1/cropped2/image0091.label \
+	92 seq1/cropped2/image0092.png seq1/cropped2/image0092.label \
+	93 seq1/cropped2/image0093.png seq1/cropped2/image0093.label \
+	94 seq1/cropped2/image0094.png seq1/cropped2/image0094.label \
+	95 seq1/cropped2/image0095.png seq1/cropped2/image0095.label \
+	96 seq1/cropped2/image0096.png seq1/cropped2/image0096.label \
+	97 seq1/cropped2/image0097.png seq1/cropped2/image0097.label \
+	98 seq1/cropped2/image0098.png seq1/cropped2/image0098.label \
+	99 seq1/cropped2/image0099.png seq1/cropped2/image0099.label \
+       100 seq1/cropped2/image0100.png seq1/cropped2/image0100.label
+
+./pickle_labels.py c3.pkl 90 seq1/cropped3/image0090.png seq1/cropped3/image0090.label \
+	91 seq1/cropped3/image0091.png seq1/cropped3/image0091.label \
+	92 seq1/cropped3/image0092.png seq1/cropped3/image0092.label \
+	93 seq1/cropped3/image0093.png seq1/cropped3/image0093.label \
+	94 seq1/cropped3/image0094.png seq1/cropped3/image0094.label \
+	95 seq1/cropped3/image0095.png seq1/cropped3/image0095.label \
+	96 seq1/cropped3/image0096.png seq1/cropped3/image0096.label \
+	97 seq1/cropped3/image0097.png seq1/cropped3/image0097.label \
+	98 seq1/cropped3/image0098.png seq1/cropped3/image0098.label \
+	99 seq1/cropped3/image0099.png seq1/cropped3/image0099.label \
+       100 seq1/cropped3/image0100.png seq1/cropped3/image0100.label
