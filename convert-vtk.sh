66b69ca65b0d536344a8179e764bed73f3fcc7c1
Jarrell Waggoner <malloc47@gmail.com>
2012-10-24 14:22:05 -0400

    error if noise sampling doesn't work

--- convert-vtk.sh
+++ convert-vtk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p syn/$1/ground/
 mkdir -p syn/$1/img/
+set -e
 ./convert_vtk.py syn/$1.vtk syn/$1/ground/
 for f in syn/$1/ground/* ; do
 	noise/sample.py noise/data.pkl $f syn/$1/img/`basename $f .label`.png
