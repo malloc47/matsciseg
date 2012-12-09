1f5c5a1be9cf5e723f21812169e5539099de49db
Jarrell Waggoner <malloc47@gmail.com>
2012-12-09 11:47:38 -0500

    beginnings of function to plot slices vs d vs scores

---
+++ d-ex.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys,os
+import numpy as np
+from figure import Score
+
+def read_score(path,sl,d=3):
+    scores = np.genfromtxt(path,dtype='float',delimiter=',')[d-1]
+    return Score(sl
+                 , scores[0]
+                 , scores[1]
+                 , scores[2]
+                 , scores[3])
+
+def main(*args):
+    dimensions = (11,50)
+    m = np.zeros(dimensions,dtype='float')
+
+    for (j,i), v in np.ndenumerate(m):
+        m = read_score('seq1/d-ex/{0}/image{1:04d}.score'.format(i,j),j)
+
+    print(m)
+
+if __name__ == '__main__':
+    sys.exit(main(*sys.argv))
