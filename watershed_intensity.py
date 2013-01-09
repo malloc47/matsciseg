1e628c61297ddc363d69adafb99c7d42b3f7edfa
Jarrell Waggoner <malloc47@gmail.com>
2013-01-09 14:15:02 -0500

    new watershed type to go with job scripts from earlier commit

---
+++ watershed_intensity.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys,os
+from matsci.data import watershed, watershed_fixed
+from numpy import genfromtxt, savetxt
+from scipy.misc import imread
+
+
+def main(*args):
+    if(len(args) < 3):
+        return 1
+
+    im = imread(args[1],flatten=True).astype('uint8')
+    labels = genfromtxt(args[2],dtype='int16')
+
+    label_out = args[3]
+
+    suppression = int(3);
+    dilation = int(0);
+
+    if(len(args) > 4):
+        suppression = int(args[4])
+
+    if(len(args) > 5):
+        dilation = int(args[5])
+
+    labels[labels==0] = -1
+
+    w = watershed_fixed(im,labels,dilation,suppression,intensity=True)
+    
+    savetxt(label_out,w,fmt='%1d')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(*sys.argv))
