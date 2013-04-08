01373fc0b45a4a16a8310835ba90e9d1249fdbe2
Jarrell Waggoner <malloc47@gmail.com>
2013-04-08 00:33:28 -0400

    graph-drawing routine
    matplotlib just can't seme to output something that fits tightly to the
    image parameter after plotting stuff on it, so it'll have to be
    postprocessed

---
+++ overlay_graph.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os,sys
+import numpy as np
+import scipy
+from scipy import ndimage
+
+import matplotlib as mpl
+mpl.use('pdf')
+import matplotlib.pyplot as plt
+# import pylab as plt
+
+import matsciskel
+from matsci.label import pts_to_label
+from matsci.adj import Adj
+
+
+def main(*args):
+    if(len(args) < 3):
+        return 1
+
+    im,im_gray =  matsciskel.read_img(args[1])
+    pts = scipy.misc.imread(args[2],flatten=True).astype('bool')
+    lbl,num = ndimage.label(pts)
+    labels = pts_to_label(pts)
+    centers = [ (t[1],t[0]) for t in 
+                ndimage.measurements.center_of_mass(pts,lbl,range(1,num+1))]
+    adj = Adj(labels).pairs()
+
+    fig = plt.figure()
+    plt.axis('off')
+    ax=fig.add_subplot(111)
+    img = plt.imshow(im)
+    dpi = fig.get_dpi()
+    fig.set_size_inches(im_gray.shape[1]/dpi,im_gray.shape[0]/dpi)
+    plt.subplots_adjust(top=1, right=1, bottom=0.0, left=0.0, wspace=0, hspace=0)
+    # extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+
+    # plot lines
+    for p in adj:
+        x = [ centers[p[0]][0] , centers[p[1]][0] ]
+        y = [ centers[p[0]][1] , centers[p[1]][1] ]
+        plt.plot(x,y,color='r',ls='-',linewidth=5)
+    # plot points
+    ax.plot(*zip(*centers),marker='o', color='r', ls='', markersize=25)
+
+    fig.savefig(args[3],bbox_inches='tight',pad_inches=0)
+    
+
+if __name__ == '__main__':
+    sys.exit(main(*sys.argv))
